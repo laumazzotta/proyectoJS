b69279e483a009f920ab30fa05e4939b59285431
Lali Mazzotta <lau.mazzotta@gmail.com>
2020-09-22 20:19:23 -0300

    desafio clase 8: DOM

--- reservas.js
+++ reservas.js
@@ -1,5 +1,6 @@
-var hoteles = ['Llao Llao Resort, Golf & Spa', 'Villa Huinid', 'Villa Beluno', 'El Casco Art Hotel', 'Hotel Panamericano', 'Design Suites Bariloche'];
+var arrayHoteles = ['Llao Llao Resort, Golf & Spa', 'Villa Huinid', 'Villa Beluno', 'El Casco Art Hotel', 'Hotel Panamericano', 'Design Suites Bariloche'];
 var huespedes = [1, 2, 3, 4];
+var arrayCategorias = ['Standard', 'Superior', 'Presidencial'];
 
 
 function Reserva(hotel, checkin, checkout, huespedes) {
@@ -21,3 +22,26 @@ console.log(reserva);
 
 const contacto = new Contacto('Laura', '12345678', 'lali@gmail.com', 'hola');
 console.log(contacto);
+
+// desafío clase DOM //
+
+// agrego las opciones de hoteles en el form
+
+var hoteles = document.createElement('option');
+var opciones = document.createTextNode(arrayHoteles)
+hoteles.appendChild(opciones);
+document.getElementById("hotel").appendChild(hoteles);
+
+// cambio color del text hero
+
+document.getElementById("home-hero").style.color = "rgba(255, 56, 93, 0.8)";
+
+// agrego listado de categorias de habitaciones
+
+var habitaciones = document.createElement('option');
+var categorias = document.createTextNode(arrayCategorias)
+habitaciones.appendChild(categorias);
+habitaciones.setAttribute('align', 'left');
+habitaciones.setAttribute('style', 'color: white');
+document.getElementById('formulario').appendChild(habitaciones);
+
