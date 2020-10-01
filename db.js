37bb21645e0cba5a008e785757f6e6402194ab4c
Lali Mazzotta <lau.mazzotta@gmail.com>
2020-10-01 17:28:18 -0300

    entrega 2

---
+++ db.js
@@ -0,0 +1,74 @@
+const datosReserva = [
+    {
+        "hotel": "Llao Llao Resort, Golf & Spa",
+        "habitacion": "Standard",
+        "tarifa": 5000
+    }, {
+        "hotel": "Llao Llao Resort, Golf & Spa",
+        "habitacion": "Lake View",
+        "tarifa": 8000
+    }, {
+        "hotel": "Llao Llao Resort, Golf & Spa",
+        "habitacion": "Royal Suite",
+        "tarifa": 10000
+    }, {
+        "hotel": "Villa Huinid",
+        "habitacion": "Standard",
+        "tarifa": 3000
+    }, {
+        "hotel": "Villa Huinid",
+        "habitacion": "Superior",
+        "tarifa": 5000
+    }, {
+        "hotel": "Villa Huinid",
+        "habitacion": "Presidencial",
+        "tarifa": 8000
+    }, {
+        "hotel": "Villa Beluno",
+        "habitacion": "Standard Suite",
+        "tarifa": 10000
+    }, {
+        "hotel": "Villa Beluno",
+        "habitacion": "Royal Suite",
+        "tarifa": 15000
+    }, {
+        "hotel": "Villa Beluno",
+        "habitacion": "Master Suite",
+        "tarifa": 20000
+    }, {
+        "hotel": "El Casco Art Hotel",
+        "habitacion": "Studio Standard",
+        "tarifa": 8000
+    }, {
+        "hotel": "El Casco Art Hotel",
+        "habitacion": "Master Suite",
+        "tarifa": 10000
+    }, {
+        "hotel": "El Casco Art Hotel",
+        "habitacion": "Studio Balcony",
+        "tarifa": 15000
+    }, {
+        "hotel": "Hotel Panamericano",
+        "habitacion": "Standard",
+        "tarifa": 5000
+    }, {
+        "hotel": "Hotel Panamericano",
+        "habitacion": "Lake View",
+        "tarifa": 8000
+    }, {
+        "hotel": "Hotel Panamericano",
+        "habitacion": "Suite Presidencial",
+        "tarifa": 15000
+    }, {
+        "hotel": "Design Suites Bariloche",
+        "habitacion": "Standard",
+        "tarifa": 6000
+    }, {
+        "hotel": "Design Suites Bariloche",
+        "habitacion": "Junior Suite",
+        "tarifa": 8000
+    }, {
+        "hotel": "Design Suites Bariloche",
+        "habitacion": "Suite de Lujo",
+        "tarifa": 10000
+    }]
--- reservas.js
+++ reservas.js
@@ -1,7 +1,3 @@
-var arrayHoteles = ['Llao Llao Resort, Golf & Spa', 'Villa Huinid', 'Villa Beluno', 'El Casco Art Hotel', 'Hotel Panamericano', 'Design Suites Bariloche'];
-var arrayHuespedes = [1, 2, 3, 4];
-var arrayCategorias = ['Standard', 'Superior', 'Presidencial'];
-
 
 function Reserva(hotel, checkin, checkout, huespedes) {
     this.hotel = hotel;
@@ -17,49 +13,122 @@ function Contacto(nombre, telefono, email, mensaje) {
     this.mensaje = mensaje;
 }
 
-const reserva = new Reserva('Villa Huinid', '17/09/2020', '20/09/2020', 2);
-console.log(reserva);
-
-const contacto = new Contacto('Laura', '12345678', 'lali@gmail.com', 'hola');
-console.log(contacto);
-
-// agrego arrayHoteles al select de hoteles //
-
 const hotelesSelect = document.getElementById("hotel");
-for(let i = 0; i < arrayHoteles.length; i++) {
-    const optionHotel = document.createElement('option');
-    optionHotel.value = i;
-    optionHotel.text = arrayHoteles[i];
-    hotelesSelect.appendChild(optionHotel);
+
+// armado select de Hoteles
+
+const hoteles = crearListado(datosReserva, "hotel");
+
+cargarSelect(hoteles, hotelesSelect);
+
+function cargarSelect(array, select) {
+	array.forEach(element => {
+		let option = document.createElement('option');
+		option.value = element;
+		option.textContent = element;
+		select.appendChild(option);
+	})
 }
 
-// agrego arrayHuespedes al select del huespedes //
+function crearListado(array, key) {
+    const listado = [];
+    
+    array.forEach(elem => {
+        if (!listado.includes(elem[key])) {
+			listado.push(elem[key]);
+		}
+    })
+    return listado.sort();
+
+}
+
+// Armado del checkin //
+const dateCheckin = new Date();
+const diaCheckin = dateCheckin.getDay().toString().padStart(2, "0");
+const mesCheckin = dateCheckin.getMonth().toString().padStart(2, "0");
+const anioCheckin = dateCheckin.getFullYear().toString();
+const fechaCheckIn = anioCheckin + '-' + mesCheckin + '-' + diaCheckin;
+const checkinSelect = document.getElementById("checkin");
+checkinSelect.value = fechaCheckIn;
+
+// Tomar valor de LocalStorage si está disponible
+if (localStorage.getItem('checkin')) {
+    checkinSelect.value = localStorage.getItem('checkin');
+}
+
+// Armado del checkout //
+const dateCheckout = new Date();
+dateCheckout.setDate(dateCheckin.getDate() + 1);
+const diaCheckout = dateCheckout.getDay().toString().padStart(2, "0");
+const mesCheckout = dateCheckout.getMonth().toString().padStart(2, "0");
+const anioCheckout = dateCheckout.getFullYear().toString();
+const fechaCheckout = anioCheckout + '-' + mesCheckout + '-' + diaCheckout;
+const checkoutSelect = document.getElementById("checkout");
+checkoutSelect.value = fechaCheckout;
+
+// Tomar valor de LocalStorage si está disponible
+if (localStorage.getItem('checkout')) {
+    checkoutSelect.value = localStorage.getItem('checkout');
+}
+
+// Armado select de huespedes //
+
+var arrayHuespedes = [1, 2, 3, 4];
 
 const huespedesSelect = document.getElementById("huespedes");
 for(let i = 0; i < arrayHuespedes.length; i++) {
     const cantidadHuespedes  = document.createElement('option');
-    cantidadHuespedes.value = i;
+    cantidadHuespedes.value = i+1;
     cantidadHuespedes.text = arrayHuespedes[i];
     huespedesSelect.appendChild(cantidadHuespedes);
 }
 
-// agrego listado de categorias de habitaciones
+// Armado lista de categorias de habitaciones y tarifas
 
-const habitaciones = document.createElement('p');
-const categorias = document.createTextNode(arrayCategorias)
-habitaciones.appendChild(categorias);
-habitaciones.setAttribute('align', 'left');
-habitaciones.setAttribute('style', 'color: grey');
-document.getElementById('categorias').appendChild(habitaciones);
+function mostrarDispo(hotel, checkin, checkout, cantHuespedes) {
 
-// EVENTOS //
+    const container = document.getElementById('categorias');
+
+    container.innerHTML = 
+    `<h1 class="text-josefin"><strong>${hotel}</strong></h1>
+    <p class="text-lato">Huespedes: ${cantHuespedes}</p> 
+    <p class="text-lato">Fecha Check in: ${checkin}</p>
+    <p class="text-lato">Fecha Check out: ${checkout}</p>`
+
+    const opciones = datosReserva.filter(elem => {
+        return elem.hotel == hotel;
+    })
+    opciones.forEach(elem => {
+        let resultado = document.createElement('div');
+		resultado.innerHTML = elem.habitacion + ' $' + elem.tarifa;
+		container.appendChild(resultado);
+    });
+
+    container.style.textAlign = "center";
+    
+}
+
+// EVENTO boton BUSCAR //
 
 const modal = document.getElementById("modal");
 const boton = document.getElementById("submit");
 const closeBtn = document.getElementsByClassName("close")[0];
 
 boton.addEventListener('click', function(){
+    const hotel = document.getElementById("hotel").value;
+    const checkin = document.getElementById("checkin").value;
+    const checkout = document.getElementById("checkout").value;
+    const cantHuespedes = document.getElementById("huespedes").value;
+
+    localStorage.setItem('hotel', hotel);
+    localStorage.setItem('checkin', checkin);
+    localStorage.setItem('checkout', checkout);
+    localStorage.setItem('cantHuespedes', cantHuespedes);
+
+    mostrarDispo(hotel, checkin, checkout, cantHuespedes)
+
     modal.style.display = "block";
+
 });
 
 closeBtn.addEventListener('click', function() {
@@ -75,3 +144,6 @@ window.onclick = function(event) {
     modal.style.display = "none";
     }
 };
+
+
+
